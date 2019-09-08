9de7cb97083d188e8ad6edc7bc9834dd6c982585
mihaidita <ditamihai@gmail.com>
2019-09-08 16:45:40 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import ListItems from './ListItems';
 import './App.css';
+import _ from 'lodash';
 
 class App extends Component {
   constructor(props) {
@@ -30,11 +32,37 @@ class App extends Component {
       ]
     }
   }
+
+  handleOnClick = id => {
+    const topics = _.cloneDeep(this.state.topics);
+
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === id) {
+        topics[i].completed = !topics[i].completed;
+        break;
+      }
+    }
+
+    this.setState({ topics: topics })
+  };
+
   render() {
     const { topics } = this.state;
     return (
       <div className="App">
         <h1>Topics</h1>
+        <ul>
+          {topics.map(topic => (
+            <ListItems
+              key={topic.id}
+              id= {topic.id}
+              name={topic.name}
+              description={topic.description}
+              completed={topic.completed}
+              handleOnClick={this.handleOnClick}
+            />
+          ))}
+        </ul>
       </div>
     )
   };
--- src/ListItems.jsx
+++ src/ListItems.jsx
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
+import ListBody from './ListBody';
 
 const styles = {
   li: {
-    display: flex;
+    display: "flex"
   },
-  leftWall: color => {
+  leftWall: color => ({
     width: "0.5em",
     backgroundColor: color
-  }
+  })
 }
 
 export default class ListItems extends Component {
-  const { completed } = this.props;
   render() {
+    const { completed, name, description, id } = this.props;
     return(
-      <li style={styles.li}>
+      <li style={styles.li} onClick={() => this.props.handleOnClick(id)}>
         <div style={styles.leftWall(completed ? 'green' : 'red')}>
-          {/* //HERE IS THE BODY */}
+            <ListBody name={name} description={description}/>
         </div>
       </li>
     );
